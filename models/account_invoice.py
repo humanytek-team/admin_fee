14dc12154655149e12af05bfceb8cdaa3925a9aa
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2020-06-09 17:45:41 -0500

    [IMP] admin_fee_percentage in sale and invoice

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -4,3 +4,15 @@ from odoo import _, api, fields, models
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
+
+    admin_fee_percentage = fields.Float(
+        string='Admin Fee %',
+        compute='_get_admin_fee_percentage',
+        store=True,
+        readonly=False,
+    )
+
+    @api.onchange('partner_id')
+    def _get_admin_fee_percentage(self):
+        for order in self:
+            order.admin_fee_percentage = order.partner_id.admin_fee_percentage
