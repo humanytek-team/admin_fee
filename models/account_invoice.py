6b93c72a6935298815e329f2fc4c840a27a1148a
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2020-06-09 18:09:50 -0500

    [IMP] views

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -11,8 +11,22 @@ class AccountInvoice(models.Model):
         store=True,
         readonly=False,
     )
+    admin_fee = fields.Monetary(
+        compute='_get_admin_fee',
+        store=True,
+    )
+    amount_with_admin_fee = fields.Monetary(
+        compute='_get_admin_fee',
+        store=True,
+    )
 
-    @api.onchange('partner_id')
+    @api.depends('partner_id')
     def _get_admin_fee_percentage(self):
         for order in self:
             order.admin_fee_percentage = order.partner_id.admin_fee_percentage
+
+    @api.depends('admin_fee_percentage', 'amount_total')
+    def _get_admin_fee(self):
+        for order in self:
+            order.admin_fee = order.admin_fee_percentage / 100 * order.amount_total
+            order.amount_with_admin_fee = order.amount_total + order.admin_fee
